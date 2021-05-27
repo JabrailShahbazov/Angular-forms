cef1992561fcb9189f09a1a4cb1e526ac134aa55
Jabrail Shahbazov <cebrail.shahbazov@gmail.com>
2021-05-27 16:50:43 +0400

    Added Default Value with form TD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,31 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') signupName: NgForm;
+  @ViewChild('f') signupForm: NgForm;
   defaultQuestion = 'pet';
   answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: 'abc@mail.com'
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName,
+        email: 'abc@mail.com'
+      }
+    });
   }
 
   onSubmit() {
-    console.log(this.signupName);
+    console.log(this.signupForm);
   }
 }
